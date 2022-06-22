a523863bda4d53b509c514534842974c17184b90
Aura Russill <auracris1996@hotmail.com>
2022-06-22 09:44:58 -0500

    REFACTOR: TennisGAme1
    se realizo refactorizacion del codigo de la clase TennisGame1, se extrajeron varios metodos condicionales y cambiaron algunas variables para hacer los metodos menos complejos

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,8 +1,9 @@
+import java.util.Objects;
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private Integer score1 = 0;
+    private Integer score2 = 0;
     private String player1Name;
     private String player2Name;
 
@@ -12,65 +13,72 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+        if (Objects.equals(playerName, "player1"))
+            score1 += 1;
         else
-            m_score2 += 1;
+            score2 += 1;
     }
 
     public String getScore() {
         String score = "";
+        if (Objects.equals(score1, score2))return getStringScore(score1);
+
+        if (score1>=4 || score2>=4) return  getMinusResult(score1, score2);
+
+        return  getResultScore(score);
+    }
+
+    private String getResultScore(String score) {
         int tempScore=0;
-        if (m_score1==m_score2)
+        for (int i = 1; i<3; i++)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+            if (i==1) tempScore = score1;
+            else { score +="-"; tempScore = score2;}
+            score = tempScore(score, tempScore);
         }
         return score;
+    }
+
+    private String tempScore(String score, int tempScore) {
+        switch(tempScore)
+        {
+            case 0:
+                score +="Love";
+                break;
+            case 1:
+                score +="Fifteen";
+                break;
+            case 2:
+                score +="Thirty";
+                break;
+            case 3:
+                score +="Forty";
+                break;
+
+        }
+        return score;
+    }
+
+    private String getMinusResult(Integer score1, Integer score2) {
+        int minusResult = score1-score2;
+        if (minusResult ==1) return "Advantage player1";
+        if (minusResult ==-1) return "Advantage player2";
+        if (minusResult >=2) return  "Win for player1";
+        return "Win for player2";
+    }
+
+    private String getStringScore(Integer score1) {
+        switch (score1)
+        {
+            case 0:
+                    return "Love-All";
+            case 1:
+                    return "Fifteen-All";
+            case 2:
+                    return "Thirty-All";
+            default:
+                    return "Deuce";
+
+        }
     }
 }
